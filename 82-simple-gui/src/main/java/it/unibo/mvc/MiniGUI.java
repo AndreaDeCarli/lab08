45a201a8904ac15331890eadd038cd9842dd5547
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2023-11-13 12:47:19 +0100

    completed part 3 of the exercise

--- 82-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 82-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -49,7 +49,7 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                result.setText("result: "+ randomGenerator.nextInt());
             }
         });
     }
